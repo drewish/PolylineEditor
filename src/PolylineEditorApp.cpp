b1db54dd8597ef519e20aa5ad8cbf8c415d78063
andrew morton <drewish@katherinehouse.com>
2017-01-16 20:05:30 -0700

    Add edge and vertex hover detection

--- src/PolylineEditorApp.cpp
+++ src/PolylineEditorApp.cpp
@@ -20,8 +20,9 @@ class PolylineEditorApp : public App {
 
 	// Using a list so the iterators are valid after insertions and removals.
 	typedef typename list<PolyLine2f>::iterator Iterator;
+	typedef typename std::pair<vec2, vec2>		Edge;
 
-	bool hasSelectedFace() { return mFaceSelected != mPolyLines.end(); };
+	bool hasSelectedFace() const { return mFaceSelected != mPolyLines.end(); };
 	void selectFace( Iterator &face ) { mFaceSelected = face; }
 	void deselectFace() { mFaceSelected = mPolyLines.end(); };
 	void selectFirstFace() { mFaceSelected = mPolyLines.begin(); }
@@ -31,10 +32,19 @@ class PolylineEditorApp : public App {
 			selectFirstFace();
 	}
 
-	bool positionOnPlane( const vec2 &mouse, vec2 &onPlane );
-	Iterator containedBy( const vec2 &onPlane );
+	// Applies the camera projection to determine where \a mouse lies on the
+	// plane. If this returns true then \a onPlane will be the result.
+	bool positionOnPlane( const vec2 &mouse, vec2 &onPlane ) const;
+	Iterator faceContainedBy( const vec2 &onPlane );
+	// Is the cursor over a vertex in the selected face (plus a \a margin)? If
+	// this returns true \a result will be the point.
+	bool isOverVertex( const vec2 &cursor, vec2 &result, float margin = 10 ) const;
+	// Is the cursor over an edge in the selected face (plus a \a margin)? If this
+	// returns true \a result will be the edge and \at will be the closest point
+	// to the cursor.
+	bool isOverEdge( const vec2 &cursor, Edge &result, vec2 &at, float margin = 10 ) const;
 
-	bool isAppending();
+	bool isAppending() const { return mPolyLines.size() && ! mPolyLines.back().isClosed(); }
 
 	list<PolyLine2f>	mPolyLines;
 	Iterator	mFaceHover;		// The face the cursor is over
@@ -63,8 +73,8 @@ void PolylineEditorApp::mouseMove( MouseEvent event )
 {
 	mMousePosition = event.getPos();
 	positionOnPlane( mMousePosition, mCursorPosition );
-	if ( ! isAppending() ) {
-		mFaceHover = this->containedBy( mCursorPosition );
+	if( ! isAppending() ) {
+		mFaceHover = this->faceContainedBy( mCursorPosition );
 	}
 }
 
@@ -81,6 +91,8 @@ void PolylineEditorApp::mouseDown( MouseEvent event )
 }
 
 void PolylineEditorApp::mouseDrag( MouseEvent event ) {
+	if( isAppending() ) return;
+
 	// Something isn't quite right here. It's dragging at half speed.
 	ivec2 delta( event.getPos() - mLastMouseDrag );
 	if( delta != ivec2( 0 ) && hasSelectedFace() ) {
@@ -104,6 +116,7 @@ void PolylineEditorApp::keyUp( KeyEvent event )
 				mPolyLines.back().setClosed();
 			break;
 		case KeyEvent::KEY_TAB:
+			// Ignore Alt+Tab or Command+Tab
 			if( ! ( event.isAltDown() || event.isMetaDown() ) )
 				selectNextFace();
 			break;
@@ -129,6 +142,8 @@ void PolylineEditorApp::draw()
 {
 	ColorA hover( 1, 0, 1, 1 );
 	ColorA selected( 0, 1, 1, 1 );
+	u_int8_t circleRadius( 20 );
+	u_int8_t circleSegments( 8 );
 
 	gl::clear( Color( 0, 0, 0 ) );
 
@@ -138,26 +153,35 @@ void PolylineEditorApp::draw()
 	if( mPolyLines.size() == 0 ) return;
 
 	bool isAppending = this->isAppending();
-
-
 	auto end = mPolyLines.end();
 	if( isAppending )
 		--end;
 
 	// Draw completed shapes
-	for ( auto p = mPolyLines.begin(); p != end; ++p ) {
+	for( auto p = mPolyLines.begin(); p != end; ++p ) {
 		gl::ScopedColor color( ColorA( 1, 1, 1, 0.5 ) );
 		gl::drawSolid( *p );
-		if ( mFaceSelected == p ) {
+		if( mFaceSelected == p ) {
 			gl::ScopedColor color( selected );
 			gl::draw( *p );
-		} else if ( mFaceHover == p ) {
+
+			vec2 vert;
+			Edge edge;
+			if( isOverVertex( mCursorPosition, vert, circleRadius ) ) {
+				gl::ScopedColor color( Color( 1, 1, 0 ) );
+				gl::drawSolidCircle( vert, circleRadius, circleSegments );
+			} else if( isOverEdge( mCursorPosition, edge, vert, circleRadius ) ) {
+				gl::ScopedColor color( Color( 1, 1, 0 ) );
+				gl::drawLine( edge.first, edge.second );
+				gl::drawSolidCircle( vert, circleRadius, circleSegments );
+			}
+		} else if( mFaceHover == p ) {
 			gl::ScopedColor color( hover );
 			gl::draw( *p );
 		}
 	}
 
-	if ( isAppending ) {
+	if( isAppending ) {
 		// Draw the incomplete shape
 		PolyLine2f copy( mPolyLines.back() );
 		copy.push_back( mCursorPosition );
@@ -172,7 +196,7 @@ void PolylineEditorApp::draw()
 	}
 }
 
-bool PolylineEditorApp::positionOnPlane( const vec2 &mouse, vec2 &onPlane )
+bool PolylineEditorApp::positionOnPlane( const vec2 &mouse, vec2 &onPlane ) const
 {
     float u = mouse.x / (float) getWindowWidth();
     float v = ( getWindowHeight() - mouse.y ) / (float) getWindowHeight();
@@ -187,14 +211,54 @@ bool PolylineEditorApp::positionOnPlane( const vec2 &mouse, vec2 &onPlane )
 }
 
 
-PolylineEditorApp::Iterator PolylineEditorApp::containedBy( const vec2 &onPlane )
+PolylineEditorApp::Iterator PolylineEditorApp::faceContainedBy( const vec2 &onPlane )
 {
 	return std::find_if( mPolyLines.begin(), mPolyLines.end(),
 		[&onPlane]( const PolyLine2f &p ) { return p.contains( onPlane ); } );
 }
 
-bool PolylineEditorApp::isAppending() {
-	return mPolyLines.size() && ! mPolyLines.back().isClosed();
+bool PolylineEditorApp::isOverVertex( const vec2 &cursor, vec2 &result, float margin ) const
+{
+	std::vector<vec2> &points = mFaceSelected->getPoints();
+	float margin2 = margin * margin;
+	for( auto const &p : points ) {
+		if( glm::distance2( p, cursor ) < margin2 ) {
+			result = p;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool PolylineEditorApp::isOverEdge( const vec2 &cursor, Edge &result, vec2 &at, float margin ) const
+{
+	std::vector<vec2> &points = mFaceSelected->getPoints();
+	float margin2 = margin * margin;
+
+	if( points.size() < 2 ) return false;
+
+	auto distanceCheck = [&]( const vec2 &a, const vec2 & b ) {
+		vec2 closest = getClosestPointLinear( a, b, cursor );
+		if( glm::distance2( closest, cursor ) < margin2 ) {
+			result.first = a;
+			result.second = b;
+			at = closest;
+			return true;
+		}
+		return false;
+	};
+
+	for( auto prev = points.begin(), curr = prev + 1; curr != points.end(); ++curr ) {
+		if( distanceCheck( *prev, *curr ) )
+			return true;
+		prev = curr;
+	}
+	if( mFaceSelected->isClosed() && points.front() != points.back() ) {
+		if( distanceCheck( points.front(), points.back() ) )
+			return true;
+	}
+
+	return false;
 }
 
 void prepareSettings( App::Settings *settings )
